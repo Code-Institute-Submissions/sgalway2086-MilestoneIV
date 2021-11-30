96eab52b98758f478b266ff1f22645463b72392f
sgalway2086 <sgalway2086@gmail.com>
2021-11-30 05:01:38 +0000

    add product type filtering

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,8 +10,15 @@ def shop(request):
 
     products = Product.objects.all()
     query = None
+    category = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
@@ -24,6 +31,7 @@ def shop(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'shop/shop.html', context)
