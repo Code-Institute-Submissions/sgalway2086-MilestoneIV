e1515476398c89f9392f47b47d3456d2d37552fb
sgalway2086 <sgalway2086@gmail.com>
2021-12-08 00:33:44 +0000

    add sizes selection and display

--- bag/contexts.py
+++ bag/contexts.py
@@ -6,19 +6,32 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-
+    grand_total = total
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        grand_total = total
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            grand_total = total
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                grand_total = total
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     context = {
         'bag_items': bag_items,
