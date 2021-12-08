f4ecfa16e0564e229f449517dc769049ca63fb2c
sgalway2086 <sgalway2086@gmail.com>
2021-12-08 05:11:51 +0000

    save before log off

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,13 +2,13 @@ from decimal import Decimal
 from django.shortcuts import get_object_or_404
 from shop.models import Product
 
+
 def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
     grand_total = total
     bag = request.session.get('bag', {})
-
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
@@ -39,5 +39,5 @@ def bag_contents(request):
         'product_count': product_count,
         'grand_total': grand_total,
         }
-        
+
     return context
