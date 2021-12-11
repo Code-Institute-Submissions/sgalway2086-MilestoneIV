5c2f718efe033e1631de31ae1bd168657bb055ed
sgalway2086 <sgalway2086@gmail.com>
2021-12-11 17:38:08 +0000

    add success page and modify function name

--- checkout/models.py
+++ checkout/models.py
@@ -25,7 +25,7 @@ class Order(models.Model):
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.delivery_cost = 5
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
