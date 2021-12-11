39161bd623448c3c820daff12357cef24b814577
sgalway2086 <sgalway2086@gmail.com>
2021-12-11 22:36:40 +0000

    add webhooks and connect to stripe

--- checkout/views.py
+++ checkout/views.py
@@ -108,3 +108,4 @@ def checkout_success(request, order_number):
     }
 
     return render(request, template, context)
+    
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,7 +11,7 @@ class StripeWH_Handler:
     def handle_payment_intent_succeeded(self, event):
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
-            status=200)   
+            status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         return HttpResponse(
