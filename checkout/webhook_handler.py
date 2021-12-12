b5f6f3ee84ac97db0b684e16fee7539ec8f4aeae
sgalway2086 <sgalway2086@gmail.com>
2021-12-12 02:54:08 +0000

    save progress on webhooks

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
+
 class StripeWH_Handler:
     def __init__(self, request):
         self.request = request
@@ -7,12 +8,12 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-
     def handle_payment_intent_succeeded(self, event):
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-
     def handle_payment_intent_payment_failed(self, event):
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -2,22 +2,20 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
+
 from checkout.webhook_handler import StripeWH_Handler
+
 import stripe
+
 
 @require_POST
 @csrf_exempt
 def webhook(request):
-    """Listen for webhooks from Stripe"""
-    # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
-
-    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
-
     try:
         event = stripe.Webhook.construct_event(
         payload, sig_header, wh_secret
@@ -37,4 +35,3 @@ def webhook(request):
     event_handler = event_map.get(event_type, handler.handle_event)
     response = event_handler(event)
     return response
-    
