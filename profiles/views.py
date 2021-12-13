77c83f8b4aebcb88e408301d5dddf5fef3b73928
sgalway2086 <sgalway2086@gmail.com>
2021-12-13 22:28:48 +0000

    add first part of profile app

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import UserProfile
+
+
 def profile(request):
-    template ='profiles/profile.html'
-    context = {}
+    profile = get_object_or_404(UserProfile, user=request.user)
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
+
     return render(request, template, context)
