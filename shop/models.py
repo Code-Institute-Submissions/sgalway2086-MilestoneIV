0735e097e9540b9101eabd8b9acab80f18d83f84
sgalway2086 <sgalway2086@gmail.com>
2021-12-16 03:51:06 +0000

    add migration for stripe price key

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Category(models.Model):
 
@@ -25,6 +25,16 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     review_quantity = models.DecimalField(default=0, max_digits=10, decimal_places=0)
+    STRIPE_PRICE_ID = models.CharField(max_length=254, null=True, blank=True)
+
 
     def __str__(self):
         return self.name
+
+class Subscription(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    customer_id = models.CharField(max_length=255)
+    subscription_id = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
