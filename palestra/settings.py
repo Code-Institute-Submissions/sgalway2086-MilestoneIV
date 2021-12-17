7b09ffda048fa68d7af1b4472feef6cba083031d
sgalway2086 <sgalway2086@gmail.com>
2021-12-17 07:19:02 +0000

    deployed heroku app and changed settings

--- palestra/settings.py
+++ palestra/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g^o4otkl^n_epi(+mry%3fjj=^g1n#ub4$cfgr#z@)xln6g4$c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestoneiv.herokuapp.com', 'localhost']
 
